ae1fb153b6e1eba274d666e68ab434cbd0e5eb0e
Mohamed Dawood <mo.ah.dawood@gmail.com>
2021-07-12 13:44:01 +0200

    ...

--- zahran/lib/main.dart
+++ zahran/lib/main.dart
@@ -59,7 +59,8 @@ class ZahranApp extends StatelessWidget {
                     ReusableLocalizations.delegate
                   ],
                   supportedLocales: TR.supportedLocales,
-                  locale: locale,
+                  // locale: locale,
+                  locale: Locale('en'),
                   routes: ScreenRouter.routes,
                   navigatorKey: ScreenRouter.key,
                   theme: ThemeGenerator.generate(),
